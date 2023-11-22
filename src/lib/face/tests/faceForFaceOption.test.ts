effa6003348704a103faba82c6543b98db7bb5f5
markforster <me@markforster.info>
2023-11-22 00:45:06 +0000

    + tidying up file

--- src/lib/face/tests/faceForFaceOption.test.ts
+++ src/lib/face/tests/faceForFaceOption.test.ts
@@ -1,12 +1,9 @@
 import { FaceOption } from '..';
 import { CubeState } from '../../../cube';
 import { IFace } from '../../../cube/IFace';
-// import { COLOURS } from '../../colours';
-import { compareArray } from '../../compareArray';
 import { newCubeState } from '../../factory';
 import { faceForFaceOption } from '../faceForFaceOption';
 import * as lib from './faceForFaceOption.lib';
-// import { INDICES_FOR_WHITE } from './faceForFaceOption.lib';
 
 describe('Getting a face for a face option', () => {
   let cubeState: CubeState;
@@ -19,44 +16,42 @@ describe('Getting a face for a face option', () => {
     // Specified by Orientation
     it('Should return the WHITE face when the TOP face is specified', () => {
       const face: IFace = faceForFaceOption(cubeState, FaceOption.TOP);
+
       expect(face.colours).toEqual(lib.ALL_WHITE_COLOURS);
       expect(face.indeces).toEqual(lib.INDICES_FOR_WHITE);
-
-      // face.positions
-      // face.normals
     });
 
     it('Should return the YELLOW face when the TOP face is specified', () => {
       const face: IFace = faceForFaceOption(cubeState, FaceOption.BOTTOM);
-      // console.log('face.colours', face.colours);
+
       expect(face.colours).toEqual(lib.ALL_YELLOW_COLOURS);
       expect(face.indeces).toEqual(lib.INDICES_FOR_YELLOW);
     });
 
     it('Should return the GREEN face when the FRONT face is specified', () => {
       const face: IFace = faceForFaceOption(cubeState, FaceOption.FRONT);
-      // console.log('face.colours', face.colours);
+
       expect(face.colours).toEqual(lib.ALL_GREEN_COLOURS);
       expect(face.indeces).toEqual(lib.INDICES_FOR_GREEN);
     });
 
     it('Should return the BLUE face when the BACK face is specified', () => {
       const face: IFace = faceForFaceOption(cubeState, FaceOption.BACK);
-      // console.log('face.colours', face.colours);
+
       expect(face.colours).toEqual(lib.ALL_BLUE_COLOURS);
       expect(face.indeces).toEqual(lib.INDICES_FOR_BLUE);
     });
 
     it('Should return the RED face when the RIGHT face is specified', () => {
       const face: IFace = faceForFaceOption(cubeState, FaceOption.RIGHT);
-      // console.log('face.colours', face.colours);
+
       expect(face.colours).toEqual(lib.ALL_RED_COLOURS);
       expect(face.indeces).toEqual(lib.INDICES_FOR_RED);
     });
 
     it('Should return the ORANGE face when the LEFT face is specified', () => {
       const face: IFace = faceForFaceOption(cubeState, FaceOption.LEFT);
-      // console.log('face.colours', face.colours);
+
       expect(face.colours).toEqual(lib.ALL_ORANGE_COLOURS);
       expect(face.indeces).toEqual(lib.INDICES_FOR_ORANGE);
     });
